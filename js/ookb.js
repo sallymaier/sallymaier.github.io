4db0dcb5a1297586aed726cf296f2bb5592fb0af
sallymaier <sallymaier@gmail.com>
2014-11-04 20:20:08 -0500

    partial copy from @bjornmeansbear

---
+++ js/ookb.js
@@ -0,0 +1,19 @@
+!function ($) {
+
+  "use strict"; // jshint ;_;
+
+  // Some Custom JS for the site
+  $(".close").click(function() {
+    $("#loader").fadeOut("slow");
+    });
+
+  // Dynamically setting colors on things based on their order?
+  //$("main ul.posts li:nth-child(6n+1)").addClass("red");
+  //$("main ul.posts li:nth-child(6n+2)").addClass("orange");
+  //$("main ul.posts li:nth-child(6n+3)").addClass("yellow");
+  //$("main ul.posts li:nth-child(6n+4)").addClass("green");
+  //$("main ul.posts li:nth-child(6n+5)").addClass("blue");
+  //$("main ul.posts li:nth-child(6n+6)").addClass("purple");
+    
+    
+}(window.jQuery);
