b98db0073169c95804bb0324c36bbc9c1402ddbe
sallymaier <sallymaier@mgial.com>
2015-04-24 18:02:32 -0400

    update json

--- thesis/js/dissection.js
+++ thesis/js/dissection.js
@@ -8,7 +8,7 @@
   // Show a random header image
   // $('header img.backgroundfill:nth-of-type('+_.random(1,$('header img.backgroundfill').length)+')').show();
 
-  $.getJSON('../thesis/json/whitespace2.json', function(data) {
+  $.getJSON('http://sallymaier.github.io/thesis/json/whitespace2.json', function(data) {
     var template = $('#whitespace-template').html();
     var html = Mustache.to_html(template, data);
     $('#pub-filter').html(html);
