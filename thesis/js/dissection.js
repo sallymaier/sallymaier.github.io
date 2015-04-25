08f0a98dbd13072f66d0846386db83fab65378a9
sallymaier <sallymaier@mgial.com>
2015-04-25 15:53:13 -0400

    updating files

--- thesis/js/dissection.js
+++ thesis/js/dissection.js
@@ -8,10 +8,10 @@
   // Show a random header image
   // $('header img.backgroundfill:nth-of-type('+_.random(1,$('header img.backgroundfill').length)+')').show();
 
-  $.getJSON('http://sallymaier.github.io/thesis/json/whitespace2.json', function(data) {
-    var template = $('#whitespace-template').html();
+  $.getJSON('https://sheetlabs.com/SALL/whitespace2', function(data) {
+    var template = $('#publist').html();
     var info = Mustache.to_html(template, data);
-    $('#pub-filter').html(info);
+    $('#pubfilter').html(info);
 
   });
 });
