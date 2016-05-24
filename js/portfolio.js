dc2618490072cf6a7b956a302b1196de40f00cf5
sallymaier <sallymaier@mgial.com>
2016-05-24 17:18:19 -0400

    updates!

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+	//pretty scroll
+	$(" a.scroll").click(function(event){
+	      event.preventDefault();
+	      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 700);
+	});
+
 	
 	var url = 'https://api.behance.net/v2/users/sallymaier/projects/?api_key=qQRlBnCFszvGLbGHpVGfwoId7RnIPLjm&per_page=25&callback=?';
 	/* some issue where no more than 25 can be returned at a time, there are ways to ask for more? https://help.behance.net/hc/communities/public/questions/202357274-Number-of-Behance-API-request-results-limited-to-25- */
@@ -49,45 +55,45 @@ $(document).ready(function() {
 	// $('#project-slides').html(info);
 
 	//carousel stuff
-		var $item = $('.carousel .item'); 
-		var $wHeight = $(window).height();
-		$item.eq(0).addClass('active');
-		$item.height($wHeight); 
-		$item.addClass('full-screen');
+		// var $item = $('.carousel .item'); 
+		// var $wHeight = $(window).height();
+		// $item.eq(0).addClass('active');
+		// $item.height($wHeight); 
+		// $item.addClass('full-screen');
 
-		$('.carousel img').each(function() {
-		  var $src = $(this).attr('src');
-		  var $color = $(this).attr('data-color');
-		  $(this).parent().css({
-		    'background-image' : 'url(' + $src + ')',
-		    'background-color' : $color
-		  });
-		  $(this).remove();
-		});
+		// $('.carousel img').each(function() {
+		//   var $src = $(this).attr('src');
+		//   var $color = $(this).attr('data-color');
+		//   $(this).parent().css({
+		//     'background-image' : 'url(' + $src + ')',
+		//     'background-color' : $color
+		//   });
+		//   $(this).remove();
+		// });
 
-		$(window).on('resize', function (){
-		  $wHeight = $(window).height();
-		  $item.height($wHeight);
-		});
+		// $(window).on('resize', function (){
+		//   $wHeight = $(window).height();
+		//   $item.height($wHeight);
+		// });
 
-		$('.carousel').carousel({
-		  interval: 6000,
-		  pause: "false"
-		});
+		// $('.carousel').carousel({
+		//   interval: 6000,
+		//   pause: "false"
+		// });
 
-		var $numberofSlides = $('.item').length;
-		var $currentSlide = Math.floor((Math.random() * $numberofSlides));
+		// var $numberofSlides = $('.item').length;
+		// var $currentSlide = Math.floor((Math.random() * $numberofSlides));
 
-		$('.carousel-indicators li').each(function(){
-		  var $slideValue = $(this).attr('data-slide-to');
-		  if($currentSlide == $slideValue) {
-		    $(this).addClass('active');
-		    $item.eq($slideValue).addClass('active');
-		  } else {
-		    $(this).removeClass('active');
-		    $item.eq($slideValue).removeClass('active');
-		  }
-		});
+		// $('.carousel-indicators li').each(function(){
+		//   var $slideValue = $(this).attr('data-slide-to');
+		//   if($currentSlide == $slideValue) {
+		//     $(this).addClass('active');
+		//     $item.eq($slideValue).addClass('active');
+		//   } else {
+		//     $(this).removeClass('active');
+		//     $item.eq($slideValue).removeClass('active');
+		//   }
+		// });
 	// });  end of templated carousel
 
 
@@ -119,6 +125,11 @@ $(document).ready(function() {
 		 var projectID  = $(this).attr('data-project-id');
 		 var projectUrl = 'https://api.behance.net/v2/projects/' + projectID + '/?api_key=qQRlBnCFszvGLbGHpVGfwoId7RnIPLjm&callback=?';
 		 console.log('trying to show ' + projectUrl + '.');
+		 $('html, body').animate({
+		        scrollTop: $("#dynamic-pages").offset().top
+		    }, 2000);
+		 $("#dynamic-pages").slideDown("slow");
+
 
 		  $.getJSON(projectUrl, function(data) {
 		    //mustache for projects
@@ -126,8 +137,9 @@ $(document).ready(function() {
 		    var info = Mustache.to_html(template, data);
 		    $('#dynamic-pages').html(info);
 
-		    $('.project-page button.close').click(function() {
-		      $(this).parent().hide();
+		    $('.close a').click(function() {
+		    	console.log('trying to close.');
+		      	$("#dynamic-pages").slideToggle("slow");
 		    });
 		  });
 		});
